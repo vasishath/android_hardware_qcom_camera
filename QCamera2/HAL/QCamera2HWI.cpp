07c1b12af275885697b29902bbd16100b602613f
Suman Mukherjee <sumam@codeaurora.org>
2016-06-13 16:26:57 +0530

    BACKPORT: QCamera2: HAL1: Flush pending video frames during stopRecording.
    During stopRecording, we need to make sure all pending video
    frames in queue need to be flushed to avoid calling video
    callback after stop.
    
    Issue:
    Framework could not handle callback after stopRecording
    or during stopRecording and mediaserver crashes.
    
    Fix:
    Reduce timestamp callback frequency after or during stopRecoding.
    Flush notify data queue for any pending video frames.
    
    [ashwin]: Adapt for msm8x74 HAL
    
    Change-Id: If514df8a34cb567136ae0dbab451ad0bfecfa228

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -2201,6 +2201,8 @@ int QCamera2HardwareInterface::stopRecording()
 {
     CDBG_HIGH("%s: E", __func__);
     int rc = stopChannel(QCAMERA_CH_TYPE_VIDEO);
+    ALOGD("%s: E", __func__);
+    m_cbNotifier.flushVideoNotifications();
 
 #ifdef HAS_MULTIMEDIA_HINTS
     if (m_pPowerModule) {
